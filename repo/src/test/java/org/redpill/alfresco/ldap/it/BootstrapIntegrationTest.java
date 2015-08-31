428f6ec9e9494e6d28faaedda3b8c7b6e341a35d
Marcus Svartmark <marcus.svartmark@redpill-linpro.com>
2015-08-31 15:00:31 +0200

    Initial commit including embedded ldap server for test

---
+++ repo/src/test/java/org/redpill/alfresco/ldap/it/BootstrapIntegrationTest.java
@@ -0,0 +1,11 @@
+package org.redpill.alfresco.ldap.it;
+
+import org.junit.Test;
+
+public class BootstrapIntegrationTest extends AbstractLdapRepoIntegrationTest {
+
+  @Test
+  public void testNothing() {
+    
+  }
+}
