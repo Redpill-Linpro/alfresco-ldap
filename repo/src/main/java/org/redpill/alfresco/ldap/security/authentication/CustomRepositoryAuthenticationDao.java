f988c833c283373c85d65c128db3fd0545736bfc
Niklas Ekman <niklas.ekman@gmail.com>
2015-10-09 15:07:40 +0200

    Added feature that makes it possible to change password for users in the configured LDAP zone

---
+++ repo/src/main/java/org/redpill/alfresco/ldap/security/authentication/CustomRepositoryAuthenticationDao.java
@@ -0,0 +1,57 @@
+package org.redpill.alfresco.ldap.security.authentication;
+
+import java.util.Set;
+
+import org.alfresco.repo.security.authentication.AuthenticationException;
+import org.alfresco.repo.security.authentication.RepositoryAuthenticationDao;
+import org.apache.log4j.Logger;
+import org.redpill.alfresco.ldap.service.LdapUserService;
+import org.springframework.util.Assert;
+
+public class CustomRepositoryAuthenticationDao extends RepositoryAuthenticationDao {
+  
+  private static final Logger LOG = Logger.getLogger(CustomRepositoryAuthenticationDao.class);
+  
+  protected String syncZoneId;
+  
+  protected LdapUserService ldapUserService;
+
+  @Override
+  public void updateUser(String userName, char[] rawPassword) throws AuthenticationException {
+    // get all the authority zones for the user
+    Set<String> authorityZones = authorityService.getAuthorityZones(userName);
+    
+    if (authorityZones.contains("AUTH.EXT." + syncZoneId)) {
+      // if the user is in the configured ${ldapMgr.syncZoneId} then it's an LDAP user
+      ldapUserService.changePassword(userName, null, String.valueOf(rawPassword));
+      
+      if (LOG.isDebugEnabled()) {
+        LOG.debug(String.format("Password changed for user '%s' in zone '%s'.", userName, syncZoneId));
+      }
+    } else {
+      // if not, then it's a regular internal user and should be handled accordingly
+      super.updateUser(userName, rawPassword);
+      
+      if (LOG.isDebugEnabled()) {
+        LOG.debug(String.format("Password changed for user '%s'.", userName));
+      }
+    }
+  }
+  
+  public void setSyncZoneId(String syncZoneId) {
+    this.syncZoneId = syncZoneId;
+  }
+  
+  public void setLdapUserService(LdapUserService ldapUserService) {
+    this.ldapUserService = ldapUserService;
+  }
+
+  @Override
+  public void afterPropertiesSet() throws Exception {
+    super.afterPropertiesSet();
+    
+    Assert.hasText(syncZoneId);
+    Assert.notNull(ldapUserService);
+  }
+
+}
